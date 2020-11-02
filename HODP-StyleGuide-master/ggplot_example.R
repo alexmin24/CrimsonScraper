195ba70b264da91bd9bba790cc9e0faabb1389f6
Alexandra Min <alexandramin@dhcp-10-250-210-70.harvard.edu>
2020-11-02 00:34:21 -0500

    Sentiment analysis

--- HODP-StyleGuide-master/ggplot_example.R
+++ HODP-StyleGuide-master/ggplot_example.R
@@ -3,17 +3,20 @@ house = read.csv("numbers.csv")
 percentages = read.csv("percentages.csv")
 totals = read.csv("totals.csv")
 total_percentages=read.csv("total_percentages.csv")
+sentiments=read.csv("sentiments.csv")
 
 # Plotting 
 a <- ggplot(data=house, aes(x=year, y=winners)) +
-  geom_line(aes(y = winners), color = "#EE3838") + 
-  geom_line(aes(y = losers), color="4B5973") +
-  geom_point() + 
-  labs(title="Candidate Name Frequency in The Crimson") +
+  geom_line(aes(y = winners, color="Winner"), color = "#EE3838") + 
+  geom_line(aes(y = losers, color="Loser"), color="4B5973") +
+  geom_point(aes(color="Winner")) + 
+  geom_point(aes(color="Loser")) + 
+  scale_color_manual(values = c("#EE3838", "#4B5973"),name="Outcome", labels = c('Winner', 'Loser')) +
+  labs(title="Article Frequency in The Crimson") +
   xlab("Year") +
-  ylab("Winning Article Numbers") +
+  ylab("Number of Articles per Candidate") +
   theme(legend.position="bottom") + 
-  labs(subtitle = "1956 - 2020") + 
+  labs(subtitle = "1956 - 2016") + 
   theme_hodp() 
 a
  
@@ -26,14 +29,16 @@ ggplotly(a)
 
 # Plotting 
 b <- ggplot(data=percentages, aes(x=year, y=winners)) +
-  geom_line(aes(y = winners), color = "#EE3838") + 
-  geom_line(aes(y = losers), color="4B5973") +
-  geom_point() + 
-  labs(title="Candidate Name Frequency in The Crimson") +
+  geom_line(aes(y = winners, color="Winner"), color = "#EE3838") + 
+  geom_line(aes(y = losers, color="Loser"), color="4B5973") +
+  geom_point(aes(color="Winner")) + 
+  geom_point(aes(color="Loser")) + 
+  scale_color_manual(values = c("#EE3838", "#4B5973"),name="Outcome", labels = c('Winner', 'Loser')) +
+  labs(title="Article Frequency in The Crimson") +
   xlab("Year") +
-  ylab("Percent of Articles per Candidate") +
+  ylab("Percentage of Articles per Candidate") +
   theme(legend.position="bottom") + 
-  labs(subtitle = "1956 - 2020") + 
+  labs(subtitle = "1956 - 2016") + 
   theme_hodp() 
 b
 
@@ -52,7 +57,7 @@ c <- ggplot(data=totals, aes(x=year, y=winners)) +
   xlab("Year") +
   ylab("Percent of Articles per Candidate") +
   theme(legend.position="bottom") + 
-  labs(subtitle = "1956 - 2020") + 
+  labs(subtitle = "1956 - 2016") + 
   theme_hodp() 
 c
 
@@ -64,14 +69,16 @@ ggplotly(c)
 
 # Plotting 
 d <- ggplot(data=total_percentages, aes(x=year, y=winners)) +
-  geom_line(aes(y = winners), color = "#EE3838") + 
-  geom_line(aes(y = losers), color="4B5973") +
-  geom_point() + 
+  geom_line(aes(y = winners, color="Winner"), color = "#EE3838") + 
+  geom_line(aes(y = losers, color="Loser"), color="4B5973") +
+  geom_point(aes(color="Winner")) + 
+  geom_point(aes(color="Loser")) + 
+  scale_color_manual(values = c("#EE3838", "#4B5973"),name="Outcome", labels = c('Winner', 'Loser')) +
   labs(title="Candidate Name Frequency in The Crimson") +
   xlab("Year") +
   ylab("Percent of Articles per Candidate") +
   theme(legend.position="bottom") + 
-  labs(subtitle = "1956 - 2020") + 
+  labs(subtitle = "1956 - 2016") + 
   theme_hodp() 
 d
 
@@ -80,3 +87,32 @@ grid::grid.raster(logo, x = 0.01, y = 0.01, just = c('left', 'bottom'), width = 
 
 # Interactive Graphic
 ggplotly(d)
+
+# Plotting 
+e <- ggplot(data=sentiments, aes(x=year, y=winners_polarity,losers_polarity)) +
+  geom_line(aes(y = winners_polarity, color="Winner"), color = "#EE3838") + 
+  geom_line(aes(y = losers_polarity, color="Loser"), color="4B5973") +
+  geom_point(aes(color="Winner")) + 
+  geom_point(aes(color="Loser")) + 
+  scale_color_manual(values = c("#EE3838", "#4B5973"),name="Outcome", labels = c('Winner', 'Loser')) +
+  labs(title="Polarity for Each Candidate") +
+  xlab("Year") +
+  ylab("Polarity") +
+  theme(legend.position="bottom") + 
+  labs(subtitle = "1956 - 2016") + 
+  theme_hodp() 
+e
+
+f <- ggplot(data=sentiments, aes(x=year, y=winners_subjectivity,losers_subjectivity)) +
+  geom_line(aes(y = winners_subjectivity, color="Winner"), color = "#EE3838") + 
+  geom_line(aes(y = losers_subjectivity, color="Loser"), color="4B5973") +
+  geom_point(aes(color="Winner")) + 
+  geom_point(aes(color="Loser")) + 
+  scale_color_manual(values = c("#EE3838", "#4B5973"),name="Outcome", labels = c('Winner', 'Loser')) +
+  labs(title="Subjectivity for Each Candidate") +
+  xlab("Year") +
+  ylab("Subjectivity") +
+  theme(legend.position="bottom") + 
+  labs(subtitle = "1956 - 2016") + 
+  theme_hodp() 
+f
--- env/crimson/crimson/analysis/test.py
+++ env/crimson/crimson/analysis/test.py
@@ -32,6 +32,11 @@ def get_freq(lst, string):
     text = TextBlob(lst_str)
     return text.words.count(string,case_sensitive=True)
 
+def get_sentiment(lst):
+    lst_str = ' '.join(map(str, lst))
+    text = TextBlob(lst_str)
+    return text.sentiment
+
 def freq_body(series):
     str_list = []
     for body in series:
@@ -42,6 +47,10 @@ def freq_body(series):
     words = sorted(str_list, key=str_list.count, reverse=True)
 
     return list(OrderedDict.fromkeys(words))
+
+dont_include=['the', 'of', 'to', 'and', 'in', 'a', 'that', 'for', 'is', 'as', 'The', 'his', 'he', 'on', 'with', 'be', 'has', 'not', 'was', 'will', 'by', 'at', 'are', 'from', 'it', 'have', 'which', 'this', 'an', 'would',
+              'but', 'more', 'had','In', 'been', 'who', 'only', 'its', 'He', 'were', 'their', 'last', 'one', 'new', 'than', 'or', 'said', 'when', 'can', 'two', 'I', 'no','out', 'most', 'all', 'they', 'should', 'these', 'if', 'however,', 'our',
+              'there', 'may', 'also','any', 'about','both', 'some', 'other', 'now', 'such', 'could', 'much', 'must', 'even', 'after', 'we', 'up', 'since', 'many', 'into',  'while', 'do', 'so', 'over', 'still', 'make', 'made', 'before', 'through', 'It',]
 
 years = [i * 4 for i in range(489, 505)]
 years_text = list(map(lambda x : str(x) + ".jsonlines", years))
@@ -74,21 +83,33 @@ filters=[
 
 filters = filters[::-1]
 
+winner_polarity=[]
+winner_subjectivity=[]
+loser_polarity=[]
+loser_subjectivity=[]
+
 dfs = list(map(lambda x: pd.read_json(x, lines=True), years_text))
 for i in range(len(dfs)):
     bodies  = dfs[i]['body']
-
     for j in range(2):
         lst = list(filter(lambda x: filters[i][j] in x, bodies))
-        #df = pd.DataFrame({'body': lst})
+
+        #frequencies = freq_body([" ".join([w for w in t.split() if not w in dont_include]) for t in lst])
+        #df = pd.DataFrame({'body': frequencies})
         #df.to_csv(str(years[i]) + filters[i][j] + ".csv" ,index=False)
 
         if j == 0:
             article_nums_winners.append(len(lst))
             total_freq_winners.append(get_freq(bodies, filters[i][j]))
+
+            winner_polarity.append(get_sentiment(lst).polarity)
+            winner_subjectivity.append(get_sentiment(lst).subjectivity)
         else:
             article_nums_losers.append(len(lst))
             total_freq_losers.append(get_freq(bodies, filters[i][j]))
+
+            loser_polarity.append(get_sentiment(lst).polarity)
+            loser_subjectivity.append(get_sentiment(lst).subjectivity)
 
 
 '''for df in dfs:
@@ -113,7 +134,7 @@ for x in range(len(years)):
    total_win_percentage.append(total_freq_winners[x] / (total_freq_winners[x] + total_freq_losers[x]))
    total_lost_percentage.append(total_freq_losers[x] / (total_freq_winners[x] + total_freq_losers[x]))
 
-df = pd.DataFrame({'year': years, 'winners': article_nums_winners, 'losers': article_nums_losers})
+'''df = pd.DataFrame({'year': years, 'winners': article_nums_winners, 'losers': article_nums_losers})
 df.to_csv("numbers.csv", index=False)
 
 df2 = pd.DataFrame({'year': years, 'winners': win_percentage, 'losers': lost_percentage})
@@ -123,7 +144,12 @@ df3 = pd.DataFrame({'year': years, 'winners': total_freq_winners, 'losers': tota
 df3.to_csv("totals.csv", index=False)
 
 df3 = pd.DataFrame({'year': years, 'winners': total_win_percentage, 'losers': total_lost_percentage})
-df3.to_csv("total_percentages.csv", index=False)
+df3.to_csv("total_percentages.csv", index=False)'''
+
+df4 = pd.DataFrame({'year': years, 'winners_polarity': winner_polarity,
+                   'winners_subjectivity': winner_subjectivity, 'losers_polarity': loser_polarity,
+                    'losers_subjectivity': loser_subjectivity})
+df4.to_csv('sentiments.csv', index=False)
 
 '''plt.plot( 'year', 'winners', data=df, marker='o', markerfacecolor='blue', markersize=12, color='skyblue', linewidth=4)
 plt.plot( 'year', 'losers', data=df, marker='', color='olive', linewidth=2)
@@ -134,12 +160,10 @@ plt.show()'''
 plt.plot('year', 'losers', data=df2, marker='o', color='blue', linewidth=2)
 plt.show()'''
 
-dont_include=['the', 'of', 'to', 'and', 'in', 'a', 'that', 'for', 'is', 'as', 'The', 'his', 'he', 'on', 'with', 'be', 'has', 'not', 'was', 'will', 'by', 'at', 'are', 'from', 'it', 'have', 'which', 'this', 'an', 'would',
-              'but', 'more', 'had','In', 'been', 'who', 'only', 'its', 'He', 'were', 'their', 'last', 'one', 'new', 'than', 'or', 'said', 'when', 'can', 'two', 'I', 'no','out', 'most', 'all', 'they', 'should', 'these', 'if', 'however,', 'our',
-              'there', 'may', 'also','any', 'about','both', 'some', 'other', 'now', 'such', 'could', 'much', 'must', 'even', 'after', 'we', 'up', 'since', 'many', 'into',  'while', 'do', 'so', 'over', 'still', 'make', 'made', 'before', 'through', 'It',]
 
 
 bodies  = dfs[0]['body']
-#print(freq_body(bodies))
-print (get_freq(bodies, 'Eisenhower'))
+#print(freq_body([" ".join([w for w in t.split() if not w in dont_include]) for t in bodies]
+#))
+#print (get_freq(bodies, 'Eisenhower'))
 dfs
