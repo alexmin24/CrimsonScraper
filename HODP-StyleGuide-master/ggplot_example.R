abcd9b30b4ddd7a1bbc084eecf48d5411c6a3fdf
Alexandra Min <alexandramin@dhcp-10-250-210-70.harvard.edu>
2020-11-01 21:10:49 -0500

    Updated R

--- HODP-StyleGuide-master/ggplot_example.R
+++ HODP-StyleGuide-master/ggplot_example.R
@@ -1,17 +1,19 @@
 source('styleguide.R')
 house = read.csv("numbers.csv")
 percentages = read.csv("percentages.csv")
+totals = read.csv("totals.csv")
+total_percentages=read.csv("total_percentages.csv")
 
 # Plotting 
 a <- ggplot(data=house, aes(x=year, y=winners)) +
-  geom_line(aes(y = winners), color = "darkred") + 
-  geom_line(aes(y = losers), color="steelblue", linetype="twodash") +
+  geom_line(aes(y = winners), color = "#EE3838") + 
+  geom_line(aes(y = losers), color="4B5973") +
   geom_point() + 
-  labs(title="Winners/Losers") +
+  labs(title="Candidate Name Frequency in The Crimson") +
   xlab("Year") +
   ylab("Winning Article Numbers") +
   theme(legend.position="bottom") + 
-  labs(subtitle = "1976 - 2018", caption = "Source: https://electionlab.mit.edu/data") + 
+  labs(subtitle = "1956 - 2020") + 
   theme_hodp() 
 a
  
@@ -24,14 +26,14 @@ ggplotly(a)
 
 # Plotting 
 b <- ggplot(data=percentages, aes(x=year, y=winners)) +
-  geom_line(aes(y = winners), color = "darkred") + 
-  geom_line(aes(y = losers), color="steelblue", linetype="twodash") +
+  geom_line(aes(y = winners), color = "#EE3838") + 
+  geom_line(aes(y = losers), color="4B5973") +
   geom_point() + 
-  labs(title="Winners/Losers") +
+  labs(title="Candidate Name Frequency in The Crimson") +
   xlab("Year") +
-  ylab("Winning Article Numbers") +
+  ylab("Percent of Articles per Candidate") +
   theme(legend.position="bottom") + 
-  labs(subtitle = "1976 - 2018", caption = "Source: https://electionlab.mit.edu/data") + 
+  labs(subtitle = "1956 - 2020") + 
   theme_hodp() 
 b
 
@@ -40,3 +42,41 @@ grid::grid.raster(logo, x = 0.01, y = 0.01, just = c('left', 'bottom'), width = 
 
 # Interactive Graphic
 ggplotly(b)
+
+# Plotting 
+c <- ggplot(data=totals, aes(x=year, y=winners)) +
+  geom_line(aes(y = winners), color = "#EE3838") + 
+  geom_line(aes(y = losers), color="4B5973") +
+  geom_point() + 
+  labs(title="Candidate Name Frequency in The Crimson") +
+  xlab("Year") +
+  ylab("Percent of Articles per Candidate") +
+  theme(legend.position="bottom") + 
+  labs(subtitle = "1956 - 2020") + 
+  theme_hodp() 
+c
+
+# Add logo
+grid::grid.raster(logo, x = 0.01, y = 0.01, just = c('left', 'bottom'), width = unit(2, 'cm'))
+
+# Interactive Graphic
+ggplotly(c)
+
+# Plotting 
+d <- ggplot(data=total_percentages, aes(x=year, y=winners)) +
+  geom_line(aes(y = winners), color = "#EE3838") + 
+  geom_line(aes(y = losers), color="4B5973") +
+  geom_point() + 
+  labs(title="Candidate Name Frequency in The Crimson") +
+  xlab("Year") +
+  ylab("Percent of Articles per Candidate") +
+  theme(legend.position="bottom") + 
+  labs(subtitle = "1956 - 2020") + 
+  theme_hodp() 
+d
+
+# Add logo
+grid::grid.raster(logo, x = 0.01, y = 0.01, just = c('left', 'bottom'), width = unit(2, 'cm'))
+
+# Interactive Graphic
+ggplotly(d)
